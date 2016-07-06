09703166dba9b9e983ffc1e9084d6437f96a29d8
Sonam <cp.cp940@gmail.com>
2016-07-05 19:31:07 -0600

    ??? maybe

--- src/main/java/io/Sonam/MasterServer.java
+++ src/main/java/io/Sonam/MasterServer.java
@@ -1,3 +1,5 @@
+package io.Sonam;
+
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
--- src/main/java/io/Sonam/MasterServerHandler.java
+++ src/main/java/io/Sonam/MasterServerHandler.java
@@ -1,3 +1,5 @@
+package io.Sonam;
+
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
