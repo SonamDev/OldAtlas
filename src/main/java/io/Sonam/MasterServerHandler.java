42af717d412d7ab570657874bfcf459c0c265e4b
Sonam <cp.cp940@gmail.com>
2016-07-13 11:56:57 -0600

    fadsffadsafsddsfa

--- src/main/java/io/Sonam/MasterServerHandler.java
+++ src/main/java/io/Sonam/MasterServerHandler.java
@@ -60,6 +60,11 @@ public class MasterServerHandler extends SimpleChannelInboundHandler<byte[]> {
     }
 
     @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("ChannelRead 1 <");
+    }
+
+    @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         cause.printStackTrace();
         ctx.close();
